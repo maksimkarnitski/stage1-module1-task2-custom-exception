7f6cc0b783e469f29c0726d01bf2dbf3a9d46776
Maksim_Karnitski <maksim.kornitsky@gmail.com>
2023-01-04 11:09:39 +0100

    Fix

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,14 +6,11 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    Student student = null;
-    try {
-      student = Student.getValueOf(studentID);
-    } catch (NullPointerException ex) {
-        ex.getMessage();
-        System.err.print(ex + String.valueOf(studentID));
+    if (Student.getValueOf(studentID) != null) {
+      return Student.getValueOf(studentID);
+    } else {
+      throw new StudentNotFoundException(studentID);
     }
-    return student;
   }
 
   public static void main(String[] args) {
--- src/main/java/com/epam/mjc/StudentNotFoundException.java
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -1,7 +1,7 @@
 package com.epam.mjc;
 
 public class StudentNotFoundException extends IllegalArgumentException {
-    public StudentNotFoundException() {
-        super("Could not find student with ID ");
+    public StudentNotFoundException(long studentId) {
+        super("Could not find student with ID " + studentId);
     }
 }
